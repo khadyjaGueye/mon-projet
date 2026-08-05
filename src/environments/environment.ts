6d7dcdae4e2b32f45d905dd7c874be5b5fbb0667
macbook pro <macbookpro@MacBook-Pro-de-macbook.local>
2026-08-05 14:00:00 +0000

    point

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     //apiUrlNode: 'http://localhost:3000/api/',
-    apiUrlNode: "momo-backend-zeta.vercel.app",
+    apiUrlNode: "momo-backend-zeta.vercel.app/",
     apiUrlRasa:'http://localhost:5005/webhooks/rest/webhook',
 };
