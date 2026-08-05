21dfd312a9f0177d83d701d919adbe24c330f3bc
macbook pro <macbookpro@MacBook-Pro-de-macbook.local>
2026-08-05 15:50:40 +0000

    interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptor/interceptor.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-     provideClientHydration(),
-     provideHttpClient(withFetch())
-    ]
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    provideHttpClient(
+      withInterceptors([authInterceptor])),
+  ]
 };
+ 
--- src/app/core/interceptor/interceptor.interceptor.ts
+++ src/app/core/interceptor/interceptor.interceptor.ts
@@ -1,5 +1,36 @@
+import { inject } from '@angular/core';
+import { PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { HttpInterceptorFn } from '@angular/common/http';
 
-export const interceptorInterceptor: HttpInterceptorFn = (req, next) => {
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const platformId = inject(PLATFORM_ID);
+
+  if (isPlatformBrowser(platformId)) {
+    const token = localStorage.getItem('token');
+
+    // Routes qui nécessitent obligatoirement un token (admin)
+    const requiresAuth = req.url.includes('/dashbord') || req.url.includes('/profil') || req.url.includes('/categories');
+
+    if (token && requiresAuth) {
+      const cloned = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next(cloned);
+    }
+
+    //  Cas des commandes : ajouter le token seulement si présent
+    if (req.url.includes('/orders') && token) {
+      const cloned = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next(cloned);
+    }
+  }
+
   return next(req);
 };
